a0b84654c15d773a6896666f52ae11b62bce55b2
universaL <jhedtrich@gmail.com>
2011-09-05 23:40:41 +0200

    dummy fixes to go from red to green

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,24 @@
 require 'factory_girl'
 
-Factory.define :user do |u|
-  u.name 'Test User'
-  u.email 'user@test.com'
-  u.password 'please'
-end
+FactoryGirl.define do
+  factory :user do
+    name 'Test User'
+    sequence(:email){|n| "user#{n}@foobar.com" }
+    password 'please1234'
+  end
+  
+  factory :event do
+    start_time 1.month.from_now
+    sequence(:title){|n| "title #{n}"}
+    description "description"
+    association :location
+    organizer { Factory(:user)}
+  end
 
+  factory :location do
+    sequence(:name) {|n| "#{n} location"}
+    address "address"
+    lat 11.11
+    lng 11.11
+  end
+end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -4,4 +4,53 @@ describe Event do
   it {should have_many(:attendances)}
   it {should belong_to(:location)}
   it {should belong_to(:organizer)}
+  
+  context "with some events" do
+    before(:each) do
+      @past = Factory(:event, :start_time => 1.day.ago)
+      @future = Factory(:event, :start_time => 1.day.from_now)
+    end
+
+    describe "upcoming scope" do
+      before(:each) do
+        @other_future = Factory(:event, :start_time => 2.days.from_now)
+        @result = Event.upcoming
+      end
+
+      it "sorts the events in ascending order by start_time" do
+        @result.should ==  [@future, @other_future]
+      end
+
+      it "includes the future event" do
+        @result.should include(@future)
+      end
+
+      it "doesn't include the past event" do
+        @result.should_not include(@past)
+      end
+
+      it "checks at midnight"
+    end # upcoming scope
+    
+    describe "previous scope" do
+      before(:each) do
+        @other_past = Factory(:event, :start_time => 2.days.ago)
+        @result = Event.previous
+      end
+      
+      it "includes past event" do
+        @result.should include(@past)
+      end
+      
+      it "doesn't include future event" do
+        @result.should_not include(@future)
+      end
+      
+      it "orders by start_time descending" do
+        @result.should == [@past, @other_past]
+      end
+      
+      it "checks at midnight"
+    end
+  end
 end
